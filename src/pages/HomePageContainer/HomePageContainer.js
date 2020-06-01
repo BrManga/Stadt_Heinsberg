69cdb0ccc1b2e5294662f393db5169d1daee71d3
Bora Manga <bora.mng525@gmail.com>
2020-06-01 12:32:22 +0200

    disclaimer added

--- src/pages/HomePageContainer/HomePageContainer.js
+++ src/pages/HomePageContainer/HomePageContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./homePageContainer.styles.scss";
+import CookieConsent from "react-cookie-consent";
+
 
 function HomePageContainer() {
   return (
@@ -22,6 +24,16 @@ function HomePageContainer() {
           Veranstaltungen
         </Link>
       </div>
+      <CookieConsent
+        location="bottom"
+        buttonText="Continue"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        Welcome to my Website! This is a student learning project. If there is any content irregularities please do not hesitate to <a style={{ textDecoration: "none", color: "#DA7E7C" }} href="mailto:bora.mng525@gmail.com"> contact</a>.
+      </CookieConsent>
     </div>
   );
 }
